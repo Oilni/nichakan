3431d3fae80ca00514043159b304172fef3c19fb
nichakan <66010914070@msu.ac.th>
2026-02-03 16:51:03 +0700

    h

--- check_login.php
+++
@@ -1,8 +0,0 @@
-<?php
-    session_start();
-    if(empty($_SESSION['aid'])){
-        echo"Access Denied";
-        echo'<meta http-equiv="refresh" content="5; url=http://10.119.195.230/h/index.php">';
-        exit;
-    }
-?>
